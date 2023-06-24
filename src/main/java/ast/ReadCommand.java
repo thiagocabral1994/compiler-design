73cc68d7e6215e0368a6a677d628d71c5b5ec1ec
Thiago do Vale Cabral <tcabral@themeetgroup.com>
2023-06-24 18:47:33 -0300

    fix: ReadCommand

--- src/main/java/ast/ReadCommand.java
+++ src/main/java/ast/ReadCommand.java
@@ -4,14 +4,14 @@ import visitor.Visitable;
 import visitor.Visitor;
 
 public class ReadCommand extends Command implements Visitable {
-  private LValue lvalue;
+  private LValueContext lvalue;
 
-  public ReadCommand(int line, int col, LValue lvalue) {
+  public ReadCommand(int line, int col, LValueContext lvalue) {
     super(line, col);
     this.lvalue = lvalue;
   }
 
-  public LValue getLValue() { return this.lvalue; }
+  public LValueContext getLValue() { return this.lvalue; }
 
   public void accept(Visitor visitor) {
     visitor.visit(this);
