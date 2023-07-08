3716e911c52d88b7d5ab365e99830c753a033b85
Thiago do Vale Cabral <thiago.cabral@ice.ufjf.br>
2023-07-08 19:00:52 -0300

    feat: SType no Expression

--- src/main/java/ast/Expression.java
+++ src/main/java/ast/Expression.java
@@ -1,6 +1,13 @@
 package ast;
 
+import util.semantic.SemanticType;
+
 public abstract class Expression extends Node {
+  private SemanticType sType;
+
+  public void setSemanticType(SemanticType type) { this.sType = type; }
+  public SemanticType getSemanticType() { return this.sType; }
+
   public Expression(int line, int col) {
     super(line, col);
   }
